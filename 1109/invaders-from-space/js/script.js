fb7a3d29851a70900e6fb0693be5458e4d7957a7
mirai0626 <0516noi.s@gmail.com>
2023-11-09 12:52:39 +0900

    1109

---
+++ 1109/invaders-from-space/js/script.js
@@ -0,0 +1,25 @@
+//魔王画像
+//変数satanにquerySelectorでロクテンマオウの画像を取得する;
+const satan = document.querySelector('.satan img');
+
+//魔王を倒すボタン
+//変数satanにquerySelectorでロクテンマオウの画像を取得する
+const satanBtn = document.querySelector('.satansBtn');
+
+//魔王を倒す
+satanBtn.addEventListener('click', function () {
+  satan.classList.toggle('hide');
+});
+
+//敵4体の画像を取得する
+const enemyImg = document.querySelectorAll('.enemy img');
+
+//敵4体のボタンを取得
+const attackBtn = document.querySelectorAll('.enemyBtn');
+
+//ボタンをクリックしたら、
+for (let i = 0; i < attackBtn.length; i++) {
+  attackBtn[i].addEventListener('click', function () {
+    enemyImg[i].classList.toggle('hide');
+  });
+}
